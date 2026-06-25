bc8dbe205cbeb42ff37a8a0be9ea575e647d52f9
e.kasyan <eugen.kasyan@stakelogic.com>
2026-06-25 15:18:21 +0300

    OrderDeliveredEvent

--- src/main/java/com/kasjan/common/event/OrderDeliveredEvent.java
+++ src/main/java/com/kasjan/common/event/OrderDeliveredEvent.java
@@ -6,7 +6,13 @@ import lombok.NoArgsConstructor;
 @Serdeable
 @NoArgsConstructor
 public class OrderDeliveredEvent extends GeneralOrderEvent {
-  public OrderDeliveredEvent(final String id) {
+  public OrderDeliveredEvent(
+      final String id,
+      final String accountId,
+      final String restaurantId,
+      final String courierId,
+      final double amount
+  ) {
     super(EventType.ORDER_DELIVERED, id);
   }
 }
