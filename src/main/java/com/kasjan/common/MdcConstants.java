7acfd3a3ae13f6de56dac4e4d671df9ae77f8670
e.kasyan <eugen.kasyan@stakelogic.com>
2026-07-19 00:51:18 +0300

    ndc constants

--- src/main/java/com/kasjan/common/MdcConstants.java
+++ src/main/java/com/kasjan/common/MdcConstants.java
@@ -3,4 +3,5 @@ package com.kasjan.common;
 public final class MdcConstants {
   public static final String X_RAY_ID = "x-ray-id";
   public static final String ORDER_ID = "order-id";
+  public static final String EXT_ACCOUNT_ID = "ext-account-id";
 }
